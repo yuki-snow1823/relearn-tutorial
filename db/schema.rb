b8ebbf8457477ee05f553971d1169cb1fc6d181d
Yuki Horikoshi <59280290+yuki-snow1823@users.noreply.github.com>
2024-10-20 22:35:09 +0000

    user migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_09_01_234057) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_19_061230) do
   create_table "microposts", force: :cascade do |t|
     t.text "content"
     t.integer "user_id"
@@ -23,6 +23,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_09_01_234057) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
